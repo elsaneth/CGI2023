74c195cbe4adc3241be49cca4148b867564f3c1b
elsaneth <elisabeth.suits@gmail.com>
2024-01-28 18:45:45 +0200

    checkouts model

---
+++ frontend/src/app/models/checkout.ts
@@ -0,0 +1,15 @@
+import {Book} from "./book";
+
+export interface Checkout {
+  id: string;
+
+  borrowerFirstName: string;
+
+  borrowerLastName: string;
+
+  borrowedBook: Book;
+
+  checkedOutDate: string;
+
+  dueDate: string;
+}
