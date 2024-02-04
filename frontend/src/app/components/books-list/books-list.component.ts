5fd3c1c939abb58e3029f92c5285d72bfeb73fa4
elsaneth <elisabeth.suits@gmail.com>
2024-02-04 18:19:23 +0200

    pagination for books list

--- frontend/src/app/components/books-list/books-list.component.ts
+++ frontend/src/app/components/books-list/books-list.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from '../../services/book.service';
 import { Observable } from 'rxjs';
-import { Page } from '../../models/page';
+import {Page, PageRequest} from '../../models/page';
 import { Book } from '../../models/book';
+import {MatTableDataSource} from "@angular/material/table";
+import {PageEvent} from "@angular/material/paginator";
+import {Sort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-books-list',
@@ -14,6 +17,15 @@ export class BooksListComponent implements OnInit {
 
   books$!: Observable<Page<Book>>;
 
+  dataSource!: MatTableDataSource<any>
+
+  pageRequest: PageRequest = {
+    pageIndex: 0,
+    pageSize: 10,
+    sort: "dueDate",
+    direction: "asc"
+  }
+
   constructor(
     private bookService: BookService,
   ) {
@@ -23,6 +35,24 @@ export class BooksListComponent implements OnInit {
     // TODO this observable should emit books taking into consideration pagination, sorting and filtering options.
     this.books$ = this.bookService.getBooks({});
     this.books$.subscribe(data => console.log('Books data:', data));
+  }
+
+  onPageChange(event: PageEvent): void {
+    this.pageRequest.pageSize = event.pageSize
+    this.pageRequest.pageIndex = event.pageIndex
+    this.loadCheckouts();
+  }
+  onSortChange(event: Sort): void {
+    this.pageRequest.sort = event.active
+    this.pageRequest.direction = event.direction
+    this.loadCheckouts()
+  }
+
+  loadCheckouts(): void {
+    this.books$ = this.bookService.getBooks(this.pageRequest)
+    this.books$.subscribe(data => {
+      this.dataSource = new MatTableDataSource<any>(data.content)
+    });
   }
 }
 
