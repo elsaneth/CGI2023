fb173640a9722fc362700bf1087a5a9ca99a80e2
elsaneth <elisabeth.suits@gmail.com>
2024-02-12 20:43:09 +0200

    routing for checkout

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { BooksListComponent } from './components/books-list/books-list.component';
 import { BookDetailComponent } from './components/book-detail/book-detail.component';
 import {CheckoutsListComponent} from "./components/checkouts-list/checkouts-list.component";
+import {CheckoutBookComponent} from "./components/checkout-book/checkout-book.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'books', pathMatch: 'full'},
   {path: 'books', component: BooksListComponent},
   {path: 'books/:id', component: BookDetailComponent},
-  {path: 'checkouts', component: CheckoutsListComponent}
+  {path: 'checkouts', component: CheckoutsListComponent},
+  {path: 'books/:id/checkout', component: CheckoutBookComponent},
+  {path: 'checkouts/:id/checkout', component: CheckoutBookComponent}
 ];
 
 @NgModule({
